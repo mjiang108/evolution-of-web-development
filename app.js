d6fee42f89050e99082d756595a302e9549e0c69
Matt Jiang <mattljiang@gmail.com>
2022-05-03 15:09:06 -0700

    create POST /

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index", { foo: "FOO" });
+});
+
+app.post("/", (req) => {
+  console.log(req.body);
 });
 
 app.listen(port, () => {
