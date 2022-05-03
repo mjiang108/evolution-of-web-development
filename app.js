6674c84626339ee57d705c10e7cdebd4f8871b03
Matt Jiang <mattljiang@gmail.com>
2022-05-03 15:16:59 -0700

    db connection

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
+mongoose.connect(process.env.DB_CONNECT, (err) => {
+  if (err) return console.log("db connection failed");
+  console.log("db connection succeeded");
+  return app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+  });
+});
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -12,8 +21,4 @@ app.get("/", (req, res) => {
 
 app.post("/", (req) => {
   console.log(req.body);
-});
-
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
 });
