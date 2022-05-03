c2fb4ed623c8c3a1100a976dcb95863dd66773b8
Matt Jiang <mattljiang@gmail.com>
2022-05-03 14:28:49 -0700

    serve index.html

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
 app.listen(port, () => {
