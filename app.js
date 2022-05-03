280a8b6dbf5c4d0cd7671c48ac21e45f8050d69b
Matt Jiang <mattljiang@gmail.com>
2022-05-03 14:58:42 -0700

    change index.html to index.ejs

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const path = require("path");
 
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "index.html"));
+  res.render("index", { foo: "FOO" });
 });
 
 app.listen(port, () => {
