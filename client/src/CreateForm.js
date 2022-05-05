2829ba6103f87fadd91ad1f86d13b42bb3fab6d6
Matt Jiang <mattljiang@gmail.com>
2022-05-05 16:49:06 -0700

    rename jsx to js

---
+++ client/src/CreateForm.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const CreateForm = () => {
+  const [newTodo, setNewTodo] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("todos", {
+      method: "POST",
+      body: JSON.stringify({
+        content: newTodo,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(() => {
+        setNewTodo("");
+      })
+      .catch((err) => console.error(err));
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <input type="submit" value="Create" />
+    </form>
+  );
+};
+
+export default CreateForm;
