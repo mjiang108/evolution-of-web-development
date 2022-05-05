8fbdf525afc4597675ede6ada0b874b760763059
Matt Jiang <mattljiang@gmail.com>
2022-05-05 16:41:51 -0700

    finish CreateForm

--- client/src/CreateForm.jsx
+++ client/src/CreateForm.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react"
+
 const CreateForm = () => {
+  const [newTodo, setNewTodo] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetch('todos', {
+      method: 'POST',
+      body: JSON.stringify({
+        content: newTodo
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(() => {
+      setNewTodo('')
+    })
+    .catch((err) => console.error(err))
+  }
   return (
-    <h1>CreateForm</h1>
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)}/>
+      <input type="submit" value="Create"/>
+    </form>
   )
 }
 
