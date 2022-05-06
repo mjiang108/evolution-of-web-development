c3a8e13bb3e3593b636d0d88b672dfd384ca80e5
Matt Jiang <mattljiang@gmail.com>
2022-05-05 21:27:53 -0700

    description

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,15 @@ const App = () => {
       <h1>Todos</h1>
       <CreateForm setRefetchToggle={setRefetchToggle} />
       <Todos todos={todos} setTodos={setTodos} />
-      <p>Description</p>
+      <p style={{ fontStyle: "italic" }}>
+        Frontend Framework - a frontend framework is an abstraction for building
+        your frontend. This means it takes care of some common frontend issues,
+        like manipulating the DOM and support for components. Thus, the
+        developer has an easier time coding, especially for bigger apps. This
+        app uses a frontend framework called React. It functions the same way as
+        our previous app using vanilla JavaScript; however, the code looks
+        completely different.
+      </p>
     </>
   );
 };
